9b8a1d81f15118983636b2bad839d2de80f8a002
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2013-07-26 14:55:07 +0200

    Implemented basics of Vector2.

--- src/cane/vector2.js
+++ src/cane/vector2.js
@@ -1,4 +1,54 @@
-Cane.Vector2 = function(component1, component2) {
-  this[0] = component1;
-  this[1] = component2;
+Cane.Vector2 = function(component0, component1) {
+  this[0] = component0;
+  this[1] = component1;
+};
+
+Cane.Vector2.prototype = {
+  add: function(vector) {
+    var component0 = this[0] + vector[0];
+    var component1 = this[1] + vector[1];
+    var result = new Cane.Vector2(component0, component1);
+    return result;
+  },
+  subtract: function(vector) {
+    var component0 = this[0] - vector[0];
+    var component1 = this[1] - vector[1];
+    var result = new Cane.Vector2(component0, component1);
+    return result;
+  },
+  divide: function(factor) {
+    var component0 = this[0]/factor;
+    var component1 = this[1]/factor;
+    var result = new Cane.Vector2(component0, component1);
+    return result;
+  },
+  normalize: function() {
+    return this.divide(this.getLength());
+  },
+  clone: function() {
+    var vector = new Cane.Vector2(this[0], this[1]);
+    return vector;
+  },
+  multiply: function(scalar) {
+    var vector = new Cane.Vector2(
+      this[0]*scalar,
+      this[1]*scalar
+    );
+    return vector;
+  },
+  isEqual: function(vector) {
+    return(
+      this[0] == vector[0] &&
+      this[1] == vector[1]
+    );
+  },
+  getLength: function() {
+    var squareSum = Math.pow(this[0], 2) + Math.pow(this[1], 2);
+    return Math.sqrt(squareSum);
+  }
+};
+
+Cane.Vector2.zero = function() {
+  var vector = new Cane.Vector2(0, 0);
+  return vector;
 };
