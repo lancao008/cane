cfc01b668628161ede235d04e7d6fd4b65ce65c3
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2013-08-02 10:11:49 +0200

    Minor bug fix: Mouse cooprdinates now works as expected even if you've scrolled the document.

--- src/cane/mouse.js
+++ src/cane/mouse.js
@@ -7,13 +7,15 @@ Cane.Mouse = function(document, canvas) {
   document.addEventListener('mousemove', this.move.bind(this));
   canvas.addEventListener('mousedown', this.press.bind(this));
   canvas.addEventListener('mouseup', this.release.bind(this));
+
+  this.document = document;
 };
 
 Cane.Mouse.prototype = Object.create(Cane.EventEmitter);
 
 Cane.Mouse.prototype.move = function(e) {
-    this.position[0] = event.x-this.canvas.offsetLeft;
-    this.position[1] = event.y-this.canvas.offsetTop;
+    this.position[0] = event.x-this.canvas.offsetLeft+this.document.body.scrollLeft;
+    this.position[1] = event.y-this.canvas.offsetTop+this.document.body.scrollTop;
 };
 
 Cane.Mouse.prototype.press = function(event) {
